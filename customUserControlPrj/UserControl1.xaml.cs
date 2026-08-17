8f1b267825add4122d392d451bd8c29b4c2fdc45
Reeeeeeeeeza <Reeeeeeeeeza@users.noreply.github.com>
2026-08-17 15:58:52 +0500

    Add custom user control and tutorial projects

---
+++ customUserControlPrj/UserControl1.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace customUserControlPrj
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set 
+            { 
+                placeholder = value; 
+                textBlock_one.Text = placeholder;
+            }
+        }
+
+
+        public void onPress(object Sender, RoutedEventArgs e)
+        {
+            textBox_one.Clear();
+        }
+
+        public void text_inp(object Sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox_one.Text))
+            {
+                textBlock_one.Visibility = Visibility.Visible;
+            }
+            else
+            {                 
+                textBlock_one.Visibility = Visibility.Hidden; }
+            }
+
+    }
+}
---
+++ tutorial4/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tutorial4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        bool flag = true;
+        public void button_onepress(object sender, RoutedEventArgs e)
+        {
+            if (flag == true)
+            {
+                label_one.Content = "pressed";
+            }
+            else
+            {
+                label_one.Content = "not pressed";
+            }
+            flag = !flag;
+        }
+    }
+}
