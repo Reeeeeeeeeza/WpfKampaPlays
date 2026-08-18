facf7b3516e22d1efec266fdf981baef0d318d07
Reeeeeeeeeza <Reeeeeeeeeza@users.noreply.github.com>
2026-08-18 15:34:35 +0500

    Data Binding with INotifyPropertyChanged

---
+++ databindingsss/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace databindingsss
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private string boundText;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set 
+            { 
+                boundText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+            }
+        }
+
+        public void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Button Clicked!";
+        }
+
+    }
+}
