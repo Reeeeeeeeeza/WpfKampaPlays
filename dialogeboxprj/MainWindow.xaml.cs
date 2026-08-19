0a7f5f3629a81270e8bd024dcd149c69f08f88ae
Reeeeeeeeeza <Reeeeeeeeeza@users.noreply.github.com>
2026-08-19 15:22:24 +0500

    Dialog boxes

---
+++ dialogeboxprj/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace dialogeboxprj
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_1_Click(object sender, RoutedEventArgs e)
+        {   
+            OpenFileDialog  openFileDialog = new OpenFileDialog();
+            openFileDialog .Filter = "C# Source Files | *.cs";
+            openFileDialog .Title = "Select a C# Source File";
+
+            bool? result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                txt_1.Text = openFileDialog.FileName;
+            }
+            else
+            {
+                txt_1.Text = "No file selected.";
+            }
+        }
+    }
+}
