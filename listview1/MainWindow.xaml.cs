860c156252e4985e3ca0a9f2581ede4949f7cec2
Reeeeeeeeeza <Reeeeeeeeeza@users.noreply.github.com>
2026-09-02 16:46:31 +0500

    listviewpractice

---
+++ listview1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace listview1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Add_btn_Click(object sender, RoutedEventArgs e)
+        {
+            lv.Items.Add(textboxx.Text);
+        }
+        public void Del_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = lv.SelectedItems;
+
+            var newItems = new ArrayList(selectedItems);
+            foreach (var item in newItems)
+            {
+                lv.Items.Remove(item);
+            }
+        }
+        public void Clr_btn_Click(object sender, RoutedEventArgs e)
+        {
+            lv.Items.Clear();
+        }
+
+    }
+}
