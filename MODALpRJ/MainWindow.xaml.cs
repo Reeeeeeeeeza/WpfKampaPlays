b0b95e68ad6fc214bee09f3ded0e7b64a4720317
Reeeeeeeeeza <Reeeeeeeeeza@users.noreply.github.com>
2026-09-09 11:26:42 +0500

    Custom Wins

---
+++ MODALpRJ/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MODALpRJ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void OpenNormal_Click(object sender, RoutedEventArgs e)
+        {
+            NormalWin normalWin = new NormalWin();
+            normalWin.Show();
+        }
+
+        public void OpenModal_Click(object sender, RoutedEventArgs e)
+        {
+            ModalWin modalWin = new ModalWin();
+            modalWin.ShowDialog ();
+        }
+    }
+}
